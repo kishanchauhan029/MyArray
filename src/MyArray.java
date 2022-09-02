e1de55dcfea36e84f7c319ee6ceb313093b1348f
Kishankumar Chauhan <kishanchauhan029@gmail.com>
2022-09-01 21:09:55 -0400

    first commit

---
+++ src/MyArray.java
@@ -0,0 +1,8 @@
+public class MyArray {
+    public static void main(String[] args) {
+        int x = 10;
+        Integer interger1 = new Integer(10);
+        int i1 = interger1.intValue();
+        System.out.println(i1);
+    }
+}
